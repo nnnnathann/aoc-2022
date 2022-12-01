148bc296d0fb17e08ffc0b0136cfdd9fa447bee2
Nathan Bleigh <nathan.bleigh@gmail.com>
2022-12-01 15:42:23 -0600

    cleanup

--- src/aoc.clj
+++ src/aoc.clj
@@ -2,17 +2,24 @@
   (:require [clojure.java.io :as io])
   (:require [clojure.string :as string]))
 
-(defn map-in [f xs]
+(defn map-in
+  "map an iterable of iterables"
+  [f xs]
   (map #(map f %) xs))
 
-(defn sum [xs]
+(defn sum
+  "sum it up!"
+  [xs]
   (reduce + xs))
 
 (defn sort-rev
+  "sort then reverse"
   [xs]
   (reverse (sort xs)))
 
-(defn read-file [day]
+(defn read-file
+  "reads a resource file based on the name, assumes text ext"
+  [day]
   (slurp (io/reader (str "resources/" day ".txt"))))
 
 (defn read-file-lines [name]
@@ -37,6 +44,17 @@
   (filter not-empty (map #(filter not-empty %) (partition-by clojure.string/blank? input))))
 
 (defn read-inputs
+  "parses an input description file into sections based on my weird little delimiter format ``` "
   [day]
   (let [content (string/split-lines (read-file day))]
     (parse-inputs content)))
+
+
+(defn run-puzzle
+  "accepts two solvers and a puzzle definition with :input1 :expect1 :real1 (and the 2 counterparts)"
+  [solver1 solver2 p]
+  (time (do (println "test match 1: " (solver1 (get p :input1)) (get p :expect1))
+            (println "real answer 1: " (solver1 (get p :real1)))
+            (println "test match 2: " (solver2 (get p :input2)) (get p :expect2))
+            (println "real answer 2: " (solver2 (get p :real2))))))
+
--- src/day01.clj
+++ src/day01.clj
@@ -2,14 +2,25 @@
   (:require aoc)
   (:require clojure.string))
 
+; description file (includes some various context and test cases)
 (def data-file (aoc/read-inputs "day01"))
 
+; input 1 file
 (defn real-input1 [] (aoc/read-file-lines "day01_input1"))
 
+; input 2 is the same as 1 the first day
 (defn real-input2 [] (aoc/read-file-lines "day01_input1"))
 
+(def day1puzzle
+  {:input1 (get data-file :input1)
+   :expect1 (read-string (get data-file :expected1))
+   :real1 (real-input1)
+   :input2 (get data-file :input2)
+   :expect2 (read-string (get data-file :expected2))
+   :real2 (real-input2)})
+
 (defn sum-line-groups
-  "reads lines separated by blanks and sums the groups"
+  "reads lines separated by blanks and sums each group"
   [input]
   (->>
    (aoc/sep-by-blank-lines input)
@@ -18,42 +29,15 @@
    (aoc/sort-rev)))
 
 (defn solve1
+  "solve the first puzzle stage"
   [input]
   (first (sum-line-groups input)))
 
-
-
 (defn solve2
+  "solve the second puzzle stage"
   [input]
   (aoc/sum (take 3 (sum-line-groups input))))
 
-(def puzzle
-  {:input1 (get data-file :input1)
-   :expect1 (read-string (get data-file :expected1))
-   :real1 (real-input1)
-   :input2 (get data-file :input2)
-   :expect2 (read-string (get data-file :expected2))
-   :real2 (real-input2)})
-
-(defn run-puzzle [p]
-  (println "test match 1: " (solve1 (get p :input1)) (get p :expect1))
-  (println "real answer 1: " (solve1 (get p :real1)))
-  (println "test match 2: " (solve2 (get p :input2)) (get p :expect2))
-  (println "real answer 2: " (solve2 (get p :real2))))
-
-
-(defn run [opts]
-  (run-puzzle puzzle))
-
-
-(defn run-bkp [opts]
-  (let [input1 (get data-file :input1)
-        expect1 (read-string (get data-file :expected1))
-        real1 (real-input1)
-        input2 (get data-file :input2)
-        expect2 (read-string (get data-file :expected2))
-        real2 (real-input2)]
-    (println "test match 1: " (solve1 input1) expect1)
-    (println "real answer 1: " (solve1 real1))
-    (println "test match 2: " (solve2 input2) expect2)
-    (println "real answer 2: " (solve2 real2))))
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
+(defn run [_opts]
+  (aoc/run-puzzle solve1 solve2 day1puzzle))
